c2bc4f48e0addcf8fdf784c66bedf3f756d06c93
Kirpal Demian <kirpal@twoboxes.com>
2025-09-23 11:02:53 -0400

    fix: remove unused platforms

--- example/android/app/src/main/kotlin/me/louismidson/ht_localization_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/me/louismidson/ht_localization_example/MainActivity.kt
@@ -2,5 +2,4 @@ package me.louismidson.ht_localization_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
